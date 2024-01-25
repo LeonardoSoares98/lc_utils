c33819ac45bc38f69e52592597e435ed0955efb0
LixeiroCharmoso <LeonardoS.98@hotmail.com>
2024-01-25 21:28:36 +0000

    v1.1.1

--- functions/client/callback.lua
+++ functions/client/callback.lua
@@ -6,12 +6,12 @@ local serverRequests = {}
 Utils.Callback.TriggerServerCallback = function(eventName, callback, ...)
 	serverRequests[RequestId] = callback
 
-	TriggerServerEvent('lc_utils:triggerServerCallback', eventName, RequestId, getResourceName(), ...)
+	TriggerServerEvent(getResourceName()..':triggerServerCallback', eventName, RequestId, getResourceName(), ...)
 
 	RequestId = RequestId + 1
 end
 
-RegisterNetEvent('lc_utils:serverCallback', function(requestId, invoker, ...)
+RegisterNetEvent(getResourceName()..':serverCallback', function(requestId, invoker, ...)
 	assert(serverRequests[requestId], ('^3Server Callback with requestId ^1%s^3 Was Called by ^1%s^3 but does not exist.^7'):format(requestId, invoker))
 
 	serverRequests[requestId](...)

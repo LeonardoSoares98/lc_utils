d571b3d0d795f1d05b3e306aaa11e0924fb8df1b
LixeiroCharmoso <LeonardoS.98@hotmail.com>
2025-03-25 13:49:37 +0000

    New target functions for lc_fuel v1.1.0

--- functions/client/target.lua
+++ functions/client/target.lua
@@ -45,7 +45,11 @@ function Utils.Target.createTargetInCoords(location_id,x,y,z,onSelectTargetOptio
 	end
 end
 
-function Utils.Target.createTargetForModel(models,onSelectTargetOptionCallback,labelText,icon,iconColor,zone_id,callbackData)
+function Utils.Target.createTargetForModel(models,onSelectTargetOptionCallback,labelText,icon,iconColor,zone_id,callbackData,canInteractTargetCallback)
+    canInteractTargetCallback = canInteractTargetCallback or function()
+        return true
+    end
+
 	if Config.custom_scripts_compatibility.target == 'ox_target' then
 		exports['ox_target']:addModel(models, {
 			{
@@ -56,6 +60,9 @@ function Utils.Target.createTargetForModel(models,onSelectTargetOptionCallback,l
 				onSelect = function()
 					onSelectTargetOptionCallback(zone_id,callbackData)
 				end,
+                canInteract = function(entity, interact_distance, coords, name, bone)
+                    return canInteractTargetCallback(entity, interact_distance)
+                end,
 			},
 		})
 	elseif Config.custom_scripts_compatibility.target == 'qb-target' then
@@ -70,6 +77,9 @@ function Utils.Target.createTargetForModel(models,onSelectTargetOptionCallback,l
 					action = function()
 						onSelectTargetOptionCallback(zone_id,callbackData)
 					end,
+                    canInteract = function(entity, interact_distance, data)
+                        return canInteractTargetCallback(entity, interact_distance)
+                    end,
 				}
 			},
 			distance = 2.5,
@@ -77,4 +87,61 @@ function Utils.Target.createTargetForModel(models,onSelectTargetOptionCallback,l
 	else
 		Utils.CustomScripts.createTargetForModel(models,onSelectTargetOptionCallback,labelText,icon,iconColor,zone_id,callbackData)
 	end
+end
+
+function Utils.Target.createTargetForBone(boneList,params,onSelectTargetOptionCallback,callbackData,canInteractTargetCallback)
+    -- Ensure required parameters exist:
+    local labelText = assert(params.labelText, "Missing labelText in Utils.Target.createTargetForBone")
+    local icon      = assert(params.icon,      "Missing icon in Utils.Target.createTargetForBone")
+    local iconColor = assert(params.iconColor, "Missing iconColor in Utils.Target.createTargetForBone")
+
+    -- Provide sensible defaults for optional parameters:
+    local zone_id   = params.zone_id  or ""
+    local distance  = params.distance or 2.5
+    canInteractTargetCallback = canInteractTargetCallback or function()
+        return true
+    end
+
+    -- Retrieve the current resource name for the zone ID:
+    local callerResource = getResourceName()
+    zone_id = callerResource .. ":" .. zone_id
+
+    if Config.custom_scripts_compatibility.target == 'ox_target' then
+        exports['ox_target']:addGlobalOption({
+            {
+                name = zone_id,
+                bones = boneList,
+                icon = icon,
+                iconColor = iconColor,
+                label = labelText,
+                distance = distance,
+                debug = false,
+                onSelect = function(data)
+                    onSelectTargetOptionCallback(zone_id,callbackData)
+                end,
+                canInteract = function(entity, interact_distance, coords, name, bone)
+                    return canInteractTargetCallback(entity, interact_distance)
+                end,
+            },
+        })
+    elseif Config.custom_scripts_compatibility.target == 'qb-target' then
+        assert(Config.framework ~= "ESX", "qb-target not available for ESX")
+        exports['qb-target']:AddTargetBone(boneList, {
+            options = {
+                {
+                    icon = icon,
+                    label = labelText,
+                    action = function(entity)
+                        onSelectTargetOptionCallback(zone_id,callbackData)
+                    end,
+                    canInteract = function(entity, interact_distance, data)
+                        return canInteractTargetCallback(entity, interact_distance)
+                    end,
+                }
+            },
+            distance = distance,
+        })
+    else
+        Utils.CustomScripts.createTargetForBone(boneList,params,onSelectTargetOptionCallback,callbackData,canInteractTargetCallback)
+    end
 end
